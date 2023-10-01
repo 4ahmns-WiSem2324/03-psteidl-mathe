c771ff010d74f864c714a5b848cb754c60730b43
Paul Steidl <62242723+PaulSteidl@users.noreply.github.com>
2023-10-01 14:57:13 +0200

    fertig

---
+++ Assets/MyGame/Scrips/SceneSelector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour
+{
+    public void Scene1()
+    {
+        SceneManager.LoadScene("CheckIsPrime");
+    }
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+
+    public void Scene4()
+    {
+        SceneManager.LoadScene("Scene4");
+    }
+}
